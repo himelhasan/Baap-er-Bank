9f4c0e93c4527571935fc77e102812a4279f5b47
himelhasan <himelhasan497@gmail.com>
2022-08-16 17:33:00 +0600

    button enabled

--- script/bank.js
+++ script/bank.js
@@ -46,26 +46,35 @@ document.getElementById("btn-deposite").addEventListener("click", function () {
 // * 
 
 1.adding event handler to withdraw button
+2. get the withdraw amount form the withdraw input field
+2.a  convert input data from sting to number with parseFloat
+3. get previous withdraw amount total
+4. Caclulate total withdraw amount total
+5. clear the input field
+6. Caclulate the total Balance Amount
  */
 
 // step -1
 document.getElementById("btn-withdraw").addEventListener("click", function () {
   console.log("withdraw btn clicked");
 
-  // get input field data from
+  //  step -2
   const withdrawField = document.getElementById("withdraw-field");
   const withdrawFieldAmount = parseFloat(withdrawField.value);
-  console.log(withdrawFieldAmount);
-
+  //  step -3
   const withdrawAmount = document.getElementById("withdraw-amount");
   const withdrawAmountTotal = parseFloat(withdrawAmount.innerText);
-  console.log(withdrawAmountTotal);
 
-  withdrawAmount.innerText = withdrawAmountTotal + withdrawFieldAmount;
-
+  // step -4
+  const updatedwithdrawAmount = withdrawAmountTotal + withdrawFieldAmount;
+  withdrawAmount.innerText = updatedwithdrawAmount;
+  // step -5
   withdrawField.value = "";
 
+  // step-6
   const balanceAmount = document.getElementById("balance-amount");
   const balanceAmountTotal = parseFloat(balanceAmount.innerText);
   balanceAmount.innerText = balanceAmountTotal - withdrawFieldAmount;
 });
+
+console.log("working");
